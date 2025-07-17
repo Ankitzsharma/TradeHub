7b78ee381e7899882cc8b5ab256788d39fde276d
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-07-17 19:50:17 +0530

    Updated Products Component

--- frontend/src/landing_page/products/Hero.js
+++ frontend/src/landing_page/products/Hero.js
@@ -2,7 +2,14 @@ import React from "react";
 
 function Hero(){
     return(
-        <h1>Hero</h1>
+        <div className='container'>
+            <div className=' p-5 text-center'>
+                <h1>Technology</h1>
+                <h5 className='text-muted'>Sleek, modern and intuitive trading platforms</h5>
+                <p>Check out our <a href='https://www.google.com' className='text-decoration-none'>investment offering <i class="fa-solid fa-arrow-right"></i> </a></p>
+            </div>
+            <hr></hr>
+        </div>
     );  
 }
 
--- frontend/src/landing_page/products/Universe.js
+++ frontend/src/landing_page/products/Universe.js
@@ -2,7 +2,71 @@ import React from 'react';
 
 function Universe(){
     return (
-        <h1>universe</h1>
+        <div className='container'>
+            <div className='p-5 text-center'>
+                <p>want to know more about our technology stack? Check out the TradeHub.tech blog.</p>
+            </div>
+            <div className='text-center'>
+                <h1>The TradeHub Universe</h1>
+                <p className='p-3'>Extend your trading and investment experience even further withour partner platforms.</p>
+
+                <div className='row pb-5 pt-4 mb-3'>
+                    <div className='col-4'>
+                        <img src='media/images/smallcaseLogo.png'></img>
+                        <h6 className='text-muted pt-3'> Thematic investment platform </h6>
+
+                    </div>
+                    <div className='col-4 '>
+                        <img src='media/images/goldenpiLogo.png' style={{width:'190px'}}></img>
+                        <h6 className='text-muted pt-3'> Bond Trading platform </h6>
+
+                    </div>
+                    <div className='col-4'>
+                        <img src='media/images/sensibullLogo.svg' style={{width:'190px'}} className=''></img>
+                        <h6 className='text-muted pt-4 mt-2'> Asset Managment </h6>
+                    </div>
+                </div>
+
+
+
+                <div className='row pb-5 mb-3 '>
+                    <div className='col-4'>
+                        <img src='media/images/smallcaseLogo.png'></img>
+                        <h6 className='text-muted pt-3'> Thematic investment platform </h6>
+
+                    </div>
+                    <div className='col-4 '>
+                        <img src='media/images/goldenpiLogo.png' style={{width:'190px'}}></img>
+                        <h6 className='text-muted pt-3'> Bond Trading platform </h6>
+
+                    </div>
+                    <div className='col-4'>
+                        <img src='media/images/sensibullLogo.svg' style={{width:'190px'}} className=''></img>
+                        <h6 className='text-muted pt-4 mt-2'> Asset Managment </h6>
+                    </div>
+                </div>
+
+
+
+
+                <div className='row pb-5 mb-3'>
+                    <div className='col-4'>
+                        <img src='media/images/smallcaseLogo.png'></img>
+                        <h6 className='text-muted pt-3'> Thematic investment platform </h6>
+
+                    </div>
+                    <div className='col-4 '>
+                        <img src='media/images/goldenpiLogo.png' style={{width:'190px'}}></img>
+                        <h6 className='text-muted pt-3'> Bond Trading platform </h6>
+
+                    </div>
+                    <div className='col-4'>
+                        <img src='media/images/sensibullLogo.svg' style={{width:'190px'}} className=''></img>
+                        <h6 className='text-muted pt-4 mt-2'> Asset Managment </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
