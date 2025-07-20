057d807b131114cc667e094ce235ab257db68795
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-07-20 23:29:01 +0530

    update Support section

--- frontend/src/landing_page/support/CreatTicket.js
+++ frontend/src/landing_page/support/CreatTicket.js
@@ -7,7 +7,7 @@ function CreatTicket(){
                 <h4 className='pb-5'>To create a ticket, select a relevant topic</h4>
 
                 <div className='col-4'>
-                    <h6 className='pb-2'><i class="fa-solid fa-circle-plus"></i> Account Opening</h6>
+                    <h5 className='pb-2'><i class="fa-solid fa-circle-plus"></i> Account Opening</h5>
                     <ul style={{lineHeight:'49px',listStyle:'none'}}>
                         <a href='' className='text-decoration-none'><li>Online Account Opening</li></a>
                         <a href='' className='text-decoration-none'><li>Offline Account Opening</li></a>
@@ -20,7 +20,7 @@ function CreatTicket(){
                 </div>
 
                 <div className='col-4'>
-                    <h6 className='pb-2 '><i class="fa-solid fa-circle-user" ></i> Login Credentials</h6>
+                    <h5 className='pb-2 '><i class="fa-solid fa-circle-user" ></i> Login Credentials</h5>
                     <ul style={{lineHeight:'49px', listStyle:'none'}}>
                         <a href='' className='text-decoration-none'><li>Account Modification and Segment Addition</li></a>
                         <a href='' className='text-decoration-none'><li>DP ID and bank details</li></a>
@@ -30,7 +30,7 @@ function CreatTicket(){
                 </div>
 
                 <div className='col-4'>
-                    <h6 className='pb-2'><i class="fa-solid fa-square-poll-vertical"></i> Margin/Leverage, Product and Order types</h6>
+                    <h5 className='pb-2'><i class="fa-solid fa-square-poll-vertical"></i> Margin/Leverage, Product and Order types</h5>
                     <ul style={{lineHeight:'49px', listStyle:'none'}}>
                         <a href='' className='text-decoration-none'><li>Kite Web and Mobile</li></a>
                         <a href='' className='text-decoration-none'><li>Trading FAQs</li></a>
@@ -49,7 +49,7 @@ function CreatTicket(){
                 
 
                 <div className='col-4'>
-                    <h6 className='pb-2'><i class="fa-solid fa-vault"></i> Funds</h6>
+                    <h5 className='pb-2'><i class="fa-solid fa-vault"></i> Funds</h5>
                     <ul style={{lineHeight:'49px',listStyle:'none'}}>
                         <a href='' className='text-decoration-none'><li>Online Account Opening</li></a>
                         <a href='' className='text-decoration-none'><li>Offline Account Opening</li></a>
@@ -62,7 +62,7 @@ function CreatTicket(){
                 </div>
 
                 <div className='col-4'>
-                    <h6 className='pb-2 '><i class="fa-solid fa-circle-notch"></i> Console</h6>
+                    <h5 className='pb-2 '><i style={{fontSize:''}} class="fa-solid fa-circle-notch"></i> Console</h5>
                     <ul style={{lineHeight:'49px', listStyle:'none'}}>
                         <a href='' className='text-decoration-none'><li>Account Modification and Segment Addition</li></a>
                         <a href='' className='text-decoration-none'><li>DP ID and bank details</li></a>
@@ -72,7 +72,7 @@ function CreatTicket(){
                 </div>
 
                 <div className='col-4'>
-                    <h6 className='pb-2 '><i class="fa-regular fa-circle"></i> Coin</h6>
+                    <h5 className='pb-2 '><i class="fa-regular fa-circle"></i> Coin</h5>
                     <ul style={{lineHeight:'49px', listStyle:'none'}}>
                         <a href='' className='text-decoration-none'><li>Kite Web and Mobile</li></a>
                         <a href='' className='text-decoration-none'><li>Trading FAQs</li></a>
