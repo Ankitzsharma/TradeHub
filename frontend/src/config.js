3c7718ec74dae1d65f09021fa3e6685bd5f17470
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-10-23 02:13:32 +0530

    Deployed Dashboard

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 export const API_URL = process.env.REACT_APP_API_URL || "https://tradehub-backend-rgpj.onrender.com";
-export const DASHBOARD_URL = process.env.REACT_APP_DASHBOARD_URL || "http://localhost:3001/";
+export const DASHBOARD_URL = process.env.REACT_APP_DASHBOARD_URL || "https://tradehub-dashboard.onrender.com";
