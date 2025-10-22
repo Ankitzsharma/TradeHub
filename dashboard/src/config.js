44127072f2b7b83071d712ebc19ea7abd212c6b5
Ankit Sharma <ankitzsharma252004@gmail.com>
2025-10-23 02:01:01 +0530

    Frontend Deplyement

--- dashboard/src/config.js
+++ dashboard/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3002";
-export const LANDING_URL = process.env.REACT_APP_LANDING_URL || "http://localhost:3000";
+export const API_URL = process.env.REACT_APP_API_URL || "https://tradehub-backend-rgpj.onrender.com";
+export const LANDING_URL = process.env.REACT_APP_LANDING_URL || "https://tradehub-frontend-mxiu.onrender.com";
